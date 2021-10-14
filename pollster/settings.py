6be5473f036786e715ea4f5d29ac69a87c7e47b6
s-moe <sarahannmoe@gmail.com>
2021-10-14 12:22:11 -0700

    revisions to files

--- pollster/settings.py
+++ pollster/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2vr4rw&&d#_c%u5=d$jg5e0tz!@l%w13$rfrw!gm8(mm@fp&e_'
-
+from decouple import config 
+SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
